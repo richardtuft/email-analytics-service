f97f8df67f875a08bf457783e0e523f9b5de3751
syymza <syymza@gmail.com>
2015-05-21 11:59:38 +0100

    Added jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
 'use strict';
 
+// Third party modules/dependencies
 let gulp = require('gulp');
 let mocha = require('gulp-mocha');
+let jshint = require('gulp-jshint');
+
+// Paths
+let files =  {
+    mochaTests: ['./tests/**/*.js'],
+    appSrc: ['./app/**/*.js']
+};
+
 
 gulp.task('test', () => {
-    return gulp.src(['./tests/**/*.js'])
+    return gulp.src(files.mochaTests)
         .pipe(mocha());
+});
+
+gulp.task('lint', function() {
+
+    return gulp.src(files.appSrc.concat(files.mochaTests))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
