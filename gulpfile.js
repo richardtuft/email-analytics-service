3c3287028966ca130545050c803f1ed3fe053c08
syymza <syymza@gmail.com>
2015-05-21 14:58:13 +0100

    Added Istanbul for coverage report

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,15 @@
 let gulp = require('gulp');
 let mocha = require('gulp-mocha');
 let jshint = require('gulp-jshint');
+var istanbul = require('gulp-istanbul');
+
 
 // Paths
 let files =  {
+    server: ['server.js'],
     mochaTests: ['./tests/**/*.js'],
     appSrc: ['./app/**/*.js']
 };
-
-
-gulp.task('test', () => {
-    return gulp.src(files.mochaTests)
-        .pipe(mocha());
-});
 
 gulp.task('lint', function() {
 
@@ -23,3 +20,20 @@ gulp.task('lint', function() {
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
+
+gulp.task('istanbul', function (cb) {
+    gulp.src(files.appSrc.concat(files.server))
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
+        .on('finish', function () {
+            gulp.src(files.mochaTests)
+                .pipe(mocha())
+                .pipe(istanbul.writeReports({
+                        reporters: [ 'lcov', 'json', 'text', 'text-summary']
+                })) // Creating the reports after tests runned
+                .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } })) // Enforce a coverage of at least 90%
+                .on('end', cb);
+        });
+});
+
+gulp.task('test', ['istanbul', 'lint']);
