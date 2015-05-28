df59fad135dba294ad77a7dd9ee6d6a8677eb37f
syymza <syymza@gmail.com>
2015-05-28 20:33:47 +0100

    Basic spoor send service implemented

--- config/env/development.js
+++ config/env/development.js
@@ -1,11 +1,15 @@
 'use strict';
 
+
 const port = process.env.PORT || 1337;
 const logLevel = process.env.LOG_LEVEL || 'info';
 const sqsQueueUrl =  process.env.SQS_QUEUE_URL || 'https://sqs.eu-west-1.amazonaws.com/371548805176/email-platform-analytics-dev';
+const spoorPostUrl = 'https://spoor-api.ft.com/px.gif';
+
 
 module.exports = {
     port: port,
     logLevel: logLevel,
-    sqsQueueUrl: sqsQueueUrl
+    sqsQueueUrl: sqsQueueUrl,
+    spoorPostUrl: spoorPostUrl
 };
