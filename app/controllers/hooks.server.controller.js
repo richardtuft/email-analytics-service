d80fdaba1af6c7cbdb3ad80b07ece3c307330440
syymza <syymza@gmail.com>
2015-05-28 12:05:11 +0100

    Added **/.DS_Store to .gitignore

--- app/controllers/hooks.server.controller.js
+++ app/controllers/hooks.server.controller.js
@@ -2,12 +2,16 @@
 
 var logger = require('winston');
 
+logger.level = 'debug'; //TODO: Use env/config
+
 exports.handlePost = (req, res) => {
 
     let eventParser = require('../utils/eventParser.server.utils');
 
     let eventsArray = req.body;
     eventsArray.map((rawEvent) => {
+        logger.debug('Raw Event: ', rawEvent);
+
         let emailEvent = eventParser.parse(rawEvent);
         logger.info('Received ' + emailEvent.type + ' event.');
     });
