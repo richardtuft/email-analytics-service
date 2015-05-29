6feab6f161fb5450552178543ec3edf7c40e1b0f
syymza <syymza@gmail.com>
2015-05-29 12:47:23 +0100

    Use heroku app endpoint for spoor

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@
 
 let port = process.env.PORT || 1337;
 let logLevel = 'info';
-const spoorPostUrl = 'https://spoor-api.ft.com/px.gif';
+const spoorPostUrl = 'https://spoor-api.herokuapp.com/px.gif';
 
 
 module.exports = {
