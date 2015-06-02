269e683ffe83b8c137d949a55ec564d8af7dddbf
syymza <syymza@gmail.com>
2015-06-02 13:41:05 +0100

    Use loggerId to identify the worker in case of more than one thread

--- TODO.md
+++ TODO.md
@@ -1,11 +1,12 @@
 ### TODOs
 | Filename | line # | TODO
 |:------|:------:|:------
-| services/spoor.server.services.js | 12 | Remove when spoor is up again
 | services/spoor.server.services.js | 33 | ? what do we do if Spoor is down?
 | utils/eventParser.server.utils.js | 10 | use EmailEvent model (and constructor)
-| utils/eventParser.server.utils.js | 14 | deal with the different event types
-| utils/eventParser.server.utils.js | 15 | what to if we get an invalid event type?
+| utils/eventParser.server.utils.js | 15 | Specific type-based event handling
+| utils/eventParser.server.utils.js | 29 | what do we want to do if we get the wrong event?
 | queues.server.service.test.js | 3 | mock endpoints
 | spoor.server.services.test.js | 3 | mock endpoints
-| spoor.server.services.test.js | 11 | Remove SKIP when spoor is up again
+| worker.js | 51 | move to app/utils
+| worker.js | 59 | move to app/utils
+| worker.js | 97 | move to app/utils
--- worker.js
+++ worker.js
@@ -31,30 +31,35 @@ memwatch.on('leak', function(info) {
 
 logger.level = config.logLevel;
 
+const loggerId = 'WORKER.JS' + process.pid;
 
 throng(start);
 
 function start () {
-    logger.info('WORKER.JS:',  process.env.NODE_ENV + ' worker started');
+
+    logger.info(loggerId,  process.env.NODE_ENV + ' worker started');
 
     process.on('SIGTERM', shutdown);
 
-    forever(moveToSpoor).then(null, (err) => {
+    forever(moveToSpoor).then(undefined).catch((err) => {
 
-        logger.error('WORKER.JS:', err);
+        logger.error(loggerId, err);
         shutdown();
 
     });
 
+    // TODO: move to app/utils
+    // Helper function to have an infinite loop using Promises
     function forever (fn) {
-        return fn().then(function () {
+        return fn().then(() => {
             return forever(fn);  // re-execute if successful
         })
     }
 
+    // TODO: move to app/utils
     function moveToSpoor() {
 
-        logger.verbose('WORKER.JS:',  'Looking for new messages to move');
+        logger.verbose(loggerId,  'Looking for new messages to move');
 
         let lastMessageFound;
 
@@ -62,7 +67,7 @@ function start () {
 
             queue.pullFromQueue()
                 .then((data) => {
-                    logger.verbose('WORKER.JS:', 'Message retrieved from the queue');
+                    logger.verbose(loggerId, 'Message retrieved from the queue');
                     lastMessageFound = data;
                     return spoor.send(lastMessageFound.body);
                 })
@@ -70,14 +75,13 @@ function start () {
                     return queue.deleteFromQueue(lastMessageFound.receiptHandle);
                 })
                 .then(() => {
-                    logger.verbose('WORKER.JS:',  'Message moved to Spoor');
+                    logger.verbose(loggerId,  'Message moved to Spoor');
                     fulfill();
-
                 })
                 .catch(function (error) {
-                    // If we have no message we want to wait for some time and then try again
+                    // If we have no message we want to try again
                     if (error instanceof NoMessageInQueue) {
-                        logger.info('WORKER.JS', error.message);
+                        logger.info(loggerId, error.message);
                         fulfill();
                     }
                     // If any other error happens, we want the loop to end
@@ -90,8 +94,9 @@ function start () {
 
     }
 
+    // TODO: move to app/utils
     function shutdown() {
-        logger.info('WORKER.JS:',  process.env.NODE_ENV + ' worker shutting down');
+        logger.info(loggerId,  process.env.NODE_ENV + ' worker shutting down');
         process.exit();
     }
 
