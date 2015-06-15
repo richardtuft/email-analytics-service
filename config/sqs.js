31eb6b3b9c8cf99765112d799875e8f9f008d3e5
syymza <syymza@gmail.com>
2015-06-15 14:36:39 +0100

    Added test sqs keys support

--- config/sqs.js
+++ config/sqs.js
@@ -6,8 +6,8 @@ const aws = require('aws-sdk');
 const WaitTimeSeconds = 20;
 const VisibilityTimeout = 30;
 const region = process.env.SQS_REGION || 'eu-west-1';
-const accessKeyId = process.env.SQS_ACCESS_KEY;
-const secretAccessKey =  process.env.SQS_SECRET_KEY;
+const accessKeyId = process.env.SQS_ACCESS_KEY || process.env.TEST_SQS_ACCESS_KEY; //Necessary for CI with Heroku
+const secretAccessKey =  process.env.SQS_SECRET_KEY || process.env.TEST_SQS_SECRET_KEY; //Necessary for CI with Heroku
 
 module.exports = new aws.SQS({
 
