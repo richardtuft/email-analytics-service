89310f419efca3d582391a39b64d03ba97e454ba
syymza <syymza@gmail.com>
2015-08-21 11:45:27 +0100

    Added Authentication to the webhooks

--- config/env/production.js
+++ config/env/production.js
@@ -6,6 +6,7 @@ const sqsQueueUrl =  process.env.SQS_QUEUE_URL;
 const spoorPostUrl = 'https://spoor-api.ft.com/ingest';
 const workers = process.env.WEB_CONCURRENCY || 1;
 const processId = process.env.DYNO;
+const authToken = process.env.AUTH_TOKEN;
 
 
 module.exports = {
@@ -14,5 +15,6 @@ module.exports = {
     workers: workers,
     logLevel: logLevel,
     sqsQueueUrl: sqsQueueUrl,
-    spoorPostUrl: spoorPostUrl
+    spoorPostUrl: spoorPostUrl,
+    authToken: authToken
 };
