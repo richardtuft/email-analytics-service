85190db742cb11a3a94f68575fba9dcd85087fe7
snozza <andrewcsnead@gmail.com>
2016-03-09 15:47:03 +0000

    remove second shutdown of queue

--- app/utils/shutdown.server.utils.js
+++ app/utils/shutdown.server.utils.js
@@ -5,13 +5,6 @@ const logger = require('../../config/logger');
 
 /* istanbul ignore next */
 module.exports = (loggerId, queue) => {
-    logger.info(loggerId,  process.env.NODE_ENV + ' shutting down');
-    queue.closeAll()
-      .then(() => {
-        process.exit();
-      })
-      .catch(err => {
-        logger.error(err);
-        process.exit();
-      });
+  logger.info(loggerId,  process.env.NODE_ENV + ' shutting down');
+  process.exit();
 };
