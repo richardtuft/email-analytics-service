7e1176c06e2ff98b247eb708c8b88d262180d1f0
syymza <syymza@gmail.com>
2016-04-12 16:58:01 +0100

    debugging mode

--- app/controllers/sparkPostHooks.server.controller.js
+++ app/controllers/sparkPostHooks.server.controller.js
@@ -22,7 +22,11 @@ module.exports = (queue) => {
 
     return (req, res) => {
 
-        res.send('OK');
+        setTimeout(() => {
+            res.send('OK');
+        }, 20000);
+        
+        logger.debug(req.header('X-Messagesystems-Batch-Id'));
         
         let batch = new Batch({ batchId: req.header('X-Messagesystems-Batch-Id') });
 
