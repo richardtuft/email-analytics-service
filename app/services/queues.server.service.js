63248c47c6a2dff140f08046a875884a8a11235e
Daniele Zanni <syymza@gmail.com>
2016-04-22 10:52:48 +0100

    Debug string event

--- app/services/queues.server.service.js
+++ app/services/queues.server.service.js
@@ -147,7 +147,7 @@ class QueueApp extends EventEmitter {
           .then(() => resolve(e))
           .catch(reject);
       }
-      logger.debug(e);
+      logger.debug(JSON.stringify(e));
       return resolve(e);
     })
     .then(() => spoor.send(JSON.stringify(e)))
