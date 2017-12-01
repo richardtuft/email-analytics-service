dcf4a14ae4a2798bcbb879333111db4d0a2ca419
Keith Rogers <git@keithrogers.eu>
2017-12-01 10:00:15 +0000

    Use n-logger/replace Papertrail with Splunk

--- config/logger.js
+++ config/logger.js
@@ -1,17 +1,1 @@
-'use strict';
-
-// External modules
-const winston = require('winston');
-
-// Internal modules
-const config = require('./config');
-
-const dsn = process.env.SENTRY_DSN;
-
-winston.level = config.logLevel;
-
-if (process.env.NODE_ENV === 'test') {
-    winston.remove(winston.transports.Console);
-}
-
-module.exports = winston;
+module.exports = require('@financial-times/n-logger').default;
