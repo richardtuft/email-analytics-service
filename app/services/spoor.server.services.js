a25646d19e3cd5cfcab454b014909c4ffde9948a
Andrew <andrewcsnead@gmail.com>
2018-01-29 06:52:31 +0000

    add query to send scout products to different spoor

--- app/services/spoor.server.services.js
+++ app/services/spoor.server.services.js
@@ -9,49 +9,51 @@ const logger = require('../../config/logger');
 
 exports.send = (event, eventId) => {
 
-    const postUrl = config.spoorPostUrl;
-    
-    return new Promise((fulfill, reject) => {
+  // we currently send to scout and ftcom orgs on spoor
+  const scout = event.context.product && event.context.product.includes('scout');
+  const postUrl = scout ? `${config.spoorPostUrl}?org=scout` : config.spoorPostUrl;
 
-        let isProduction = process.env.NODE_ENV === 'production';
-        let length = new Buffer(event).length;
-        
-        let headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Content-Length': length,
-            "spoor-region": Math.round(Math.random()) ? 'EU' : 'US', //Randomly assign a different region
-            'User-Agent': 'ft-email-service/v1.1'
-        };
-        
-        if (eventId) {
-            headers['spoor-ticket'] = 'email-analytics-' + eventId;
+  return new Promise((fulfill, reject) => {
+
+    let isProduction = process.env.NODE_ENV === 'production';
+    let length = new Buffer(event).length;
+
+    let headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Content-Length': length,
+      "spoor-region": Math.round(Math.random()) ? 'EU' : 'US', //Randomly assign a different region
+      'User-Agent': 'ft-email-service/v1.1'
+    };
+
+    if (eventId) {
+      headers['spoor-ticket'] = 'email-analytics-' + eventId;
+    }
+
+    if (!isProduction) {
+      headers['spoor-test'] =  'true';
+    }
+
+    fetch(postUrl, {
+      method: 'post',
+      headers,
+      body: event
+    })
+      .then((res) => {
+
+        /* istanbul ignore else */
+        if (res.status >= 200 && res.status < 300) {
+          logger.debug('Spoor Header', {header: res.headers.get('spoor-ticket')});
+          fulfill(res);
         }
-        
-        if (!isProduction) {
-            headers['spoor-test'] =  'true';
+        else {
+          //TODO? what do we do if Spoor is down?
+          throw('Spoor responded with the wrong status:' + res.status);
         }
 
-        fetch(postUrl, {
-            method: 'post',
-            headers,
-            body: event
-        })
-        .then((res) => {
-
-            /* istanbul ignore else */
-            if (res.status >= 200 && res.status < 300) {
-                logger.debug('Spoor Header', {header: res.headers.get('spoor-ticket')});
-                fulfill(res);
-            }
-            else {
-                //TODO? what do we do if Spoor is down?
-                throw('Spoor responded with the wrong status:' + res.status);
-            }
-
-         })
-        .catch(reject);
-    });
+      })
+      .catch(reject);
+  });
 
 
 };
